1b858a7b4b13382f500d33bb3b8a47821bddfb7e
HARUYAMA Seigo <haruyama@unixuser.org>
2013-01-19 23:39:28 +0900

    use TIO.hGetContents (cf. https://gist.github.com/4571530/)

--- CommandLine/mygrep/Main.hs
+++ CommandLine/mygrep/Main.hs
@@ -1,7 +1,7 @@
 import System.Environment
 import System.IO
 import System.Exit
-import Control.Monad (unless, when)
+import Control.Monad (filterM)
 import qualified Data.Text as T
 import qualified Data.Text.ICU.Regex as TIR
 import qualified Data.Text.IO as TIO
@@ -9,16 +9,19 @@ import qualified Data.Text.IO as TIO
 main :: IO ()
 main = do
     args <- getArgs
-    if length args < 2
+    if length args < 1
         then do
-            hPutStrLn stderr "Usage: mygrep pattern file1 file2 ..."
+            hPutStrLn stderr "Usage: mygrep pattern [file1] [file2] ..."
             exitWith (ExitFailure 1)
         else
             do
                 reg  <- TIR.regex [] (T.pack . head $ args)
-                mapM_ (\fn ->
-                        withFile fn ReadMode $ \inh ->
-                            mainloop reg inh) $ drop 1 args
+                if length args == 1
+                    then doesGrepLinesFromHandle reg stdin
+                    else
+                        mapM_ (\fn ->
+                                withFile fn ReadMode $ \inh ->
+                                    doesGrepLinesFromHandle reg inh) $ drop 1 args
                 exitSuccess
 
 matchesLine:: TIR.Regex -> T.Text -> IO Bool
@@ -28,21 +31,9 @@ matchesLine reg line =
         TIR.find reg 0
 
 
-mainloop :: TIR.Regex -> Handle -> IO ()
-mainloop reg inh =
+doesGrepLinesFromHandle:: TIR.Regex -> Handle -> IO ()
+doesGrepLinesFromHandle reg inh =
     do
-        ineof <- hIsEOF inh
-        unless ineof (do
-                        line <- TIO.hGetLine inh
-                        b <- matchesLine reg line
-                        when b (TIO.putStrLn line)
-                        mainloop reg inh)
--- import control.monad (unless, filterM)
--- mainloop reg inh =
---     do
---         ineof <- hIsEOF inh
---         unless ineof (do
---                         ls <- TIO.hGetContents inh
---                         matchLines <- filterM (matchesLine reg) (T.lines ls)
---                         mapM_ TIO.putStrLn  matchLines
---                         mainloop reg inh)
+        contents <- TIO.hGetContents inh
+        filtered <- filterM (matchesLine reg) $ T.lines contents
+        mapM_ TIO.putStrLn filtered
