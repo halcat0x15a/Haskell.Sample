ae54f967b55c5c297b9920bdc910b604fb7a3653
HARUYAMA Seigo <haruyama@unixuser.org>
2013-01-19 13:42:06 +0900

    can take multiple files.

--- CommandLine/mygrep/Main.hs
+++ CommandLine/mygrep/Main.hs
@@ -1,30 +1,39 @@
-import qualified System.Environment as SE
-import qualified System.IO as SIO
+import System.Environment
+import System.IO
+import System.Exit
+import Control.Monad (unless, when)
 import qualified Data.Text as T
 import qualified Data.Text.ICU.Regex as TIR
 import qualified Data.Text.IO as TIO
-import Control.Monad (unless, when)
 
 main :: IO ()
 main = do
-    args <- SE.getArgs
-    reg  <- TIR.regex [] (T.pack . head $ args)
-    SIO.withFile (args !! 1) SIO.ReadMode $ \inh ->
-        mainloop reg inh
+    args <- getArgs
+    if length args < 2
+            then do
+                hPutStrLn stderr "Usage: mygrep pattern file1 file2 ..."
+                exitWith (ExitFailure 1)
+            else
+                do
+                    reg  <- TIR.regex [] (T.pack . head $ args)
+                    mapM_ (\fn -> 
+                            withFile fn ReadMode $ \inh ->
+                                mainloop reg inh) $ drop 1 args
+                    exitSuccess
 
-grep :: TIR.Regex -> T.Text -> IO Bool
-grep reg line =
+matchLine:: TIR.Regex -> T.Text -> IO Bool
+matchLine reg line =
     do
         TIR.setText reg line
         TIR.find reg 0
 
 
-mainloop :: TIR.Regex -> SIO.Handle -> IO ()
+mainloop :: TIR.Regex -> Handle -> IO ()
 mainloop reg inh =
     do
-        ineof <- SIO.hIsEOF inh
+        ineof <- hIsEOF inh
         unless ineof (do
                         line <- TIO.hGetLine inh
-                        b <- grep reg line
+                        b <- matchLine reg line
                         when b (TIO.putStrLn line)
                         mainloop reg inh)
